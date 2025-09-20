dd0d995a1d186f598530a4d96646f2328b673cf9
Itsqy <syatriarifqi@gmail.com>
2025-09-20 14:17:06 +0700

    adjustment in second of videoplayer

--- bias/Sources/Domain/Entities/skintoneinstruction.swift
+++ bias/Sources/Domain/Entities/skintoneinstruction.swift
@@ -10,5 +10,18 @@ import SwiftUI
 struct SkinToneInstruction {
     let title: String
     let instruction: String
-    let duration: TimeInterval
+    let duration: Double
+    var startTime: Double = 0
+    var endTime: Double = 0
+}
+
+extension Double {
+    /// Format seconds into mm:ss or mm:ss.SS
+    func toTimeString() -> String {
+        let totalSeconds = Int(self)
+        let minutes = totalSeconds / 60
+        let seconds = totalSeconds % 60
+        let fraction = Int((self - Double(totalSeconds)) * 100) // 2 decimals
+        return String(format: "%02d:%02d.%02d", minutes, seconds, fraction)
+    }
 }
