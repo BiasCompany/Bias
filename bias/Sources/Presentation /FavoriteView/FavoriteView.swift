5a62a03c607d90af7fe567d5e8bd4484f889f4eb
Shafa' Tiara Tsabita Himawan <81928286+glxy-sn@users.noreply.github.com>
2025-09-22 05:28:49 +0700

    feat: slicing favorite card with data dummy

--- bias/Sources/Presentation /FavoriteView/FavoriteView.swift
+++ bias/Sources/Presentation /FavoriteView/FavoriteView.swift
@@ -1,2 +1,31 @@
+//
+//  FavoriteView.swift
+//  bias
+//
+//  Created by Shafa Tiara Tsabita Himawan on 21/09/25.
+//
+
+// Presentation/Views/FavoriteView.swift
+import SwiftUI
+import Kingfisher
+
+struct FavoriteView: View {
+    @EnvironmentObject var viewModel: FavoriteViewModel
+
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 20) {
+                ForEach(viewModel.favorite) { rec in
+                    FavoriteRow(rec: rec)
+                        .padding(.horizontal, 20)
+                }
+            }
+            .padding(.vertical, 16)
+        }
+    }
+}
 
 
+#Preview("History Slicing") {
+    ShadeHistoryListView()
+}
