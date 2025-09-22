d7e07b57d8cd720110dcd1e1cadcc378ff594fda
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2025-09-22 12:22:01 +0700

    fix: detail shade view

--- lumi/Sources/Presentation /DetailShadeView/DetailShadeViewmodel.swift
+++ lumi/Sources/Presentation /DetailShadeView/DetailShadeViewmodel.swift
@@ -1,3 +1,5 @@
+
+
 import SwiftUI
 import SwiftData
 
@@ -5,10 +7,16 @@ final class DetailShadeViewModel: ObservableObject {
     @Published var showUnfavoriteAlert: Bool = false
     @Published var isFavorite: Bool = false
     @Published var showUnfavoriteDialog : Bool = false
+    @Published var isEditingNotes : Bool = false
+    @Published var editedNotes : String = ""
     let recommendation: ShadeRecommendation
-    
-    
-    
+
+    init(recommendation: ShadeRecommendation) {
+        self.recommendation = recommendation
+        self.editedNotes = recommendation.notes
+    }
+
+
     var percentage: Int { recommendation.percentage }
     var skinToneColor: Color {
         Color(hex: recommendation.skinTone.hex)
@@ -16,10 +24,8 @@ final class DetailShadeViewModel: ObservableObject {
     var shadeColor: Color {
         Color(hex: recommendation.shade.hexShade)
     }
-    init(recommendation: ShadeRecommendation) {
-        self.recommendation = recommendation
-    }
-    
+
+
     func toggleFavorite() {
         if isFavorite {
             showUnfavoriteAlert = true
@@ -32,5 +38,18 @@ final class DetailShadeViewModel: ObservableObject {
         isFavorite = false
         showUnfavoriteAlert = false
     }
+
+    func startEditingNotes(){
+        isEditingNotes = true
+        editedNotes = recommendation.notes
+
+    }
+
+    func savenotes(){
+        recommendation.notes = editedNotes
+        isEditingNotes = false
+    }
+
+
 }
 
