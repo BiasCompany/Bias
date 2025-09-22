3f35eec91edd7f9f6299b4f33a18b8edbd995214
Itsqy <syatriarifqi@gmail.com>
2025-09-23 02:25:00 +0700

    feat: add vision and image colorpicker

--- lumi/Sources/Helper/Color.swift
+++ lumi/Sources/Helper/Color.swift
@@ -1,1 +1,12 @@
 import SwiftUI
+extension Color {
+    func toHexString() -> String {
+        let uiColor = UIColor(self)
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        uiColor.getRed(&r, green: &g, blue: &b, alpha: &a)
+        return String(format: "#%02lX%02lX%02lX",
+                      lroundf(Float(r * 255)),
+                      lroundf(Float(g * 255)),
+                      lroundf(Float(b * 255)))
+    }
+}
