84f9ddba33c788cb23dc9339c901d25ae26e18ca
Shafa' Tiara Tsabita Himawan <81928286+glxy-sn@users.noreply.github.com>
2025-09-23 02:35:22 +0700

    fix: fix some conflict in project.swift about camera usage desc

--- Project.swift
+++ Project.swift
@@ -26,6 +26,7 @@ let project = Project(
                     "UISupportedInterfaceOrientations": [
                         "UIInterfaceOrientationPortrait"
                     ],
+                    "NSCameraUsageDescription": "We need access to your camera to capture your photo. This will allow the app to analyze your skin tone and suggest the right foundation shade. Your photo is only used for analysis and won’t be stored or shared",
                     "UISupportedInterfaceOrientations~ipad": [
                         "UIInterfaceOrientationPortrait",
                         "UIInterfaceOrientationPortraitUpsideDown",
