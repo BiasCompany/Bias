c1b45ca39aa735b0bd30240d34f4b8db64b0ca03
Shafa' Tiara Tsabita Himawan <81928286+glxy-sn@users.noreply.github.com>
2025-09-22 10:54:11 +0700

    feat: done slicing card best matches

--- bias/Sources/Presentation /ShadeRecommendationView/ShadeRecommendationView.swift
+++ bias/Sources/Presentation /ShadeRecommendationView/ShadeRecommendationView.swift
@@ -1,2 +1,13 @@
 
 
+import SwiftUI
+import Kingfisher
+
+struct ShadeRecommendationView: View {
+    @EnvironmentObject var viewModel: ShadeRecommendationViewModel
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
--- bias/Sources/Presentation /ShadeRecommendationView/ShadeRecommendationViewmodel.swift
+++ bias/Sources/Presentation /ShadeRecommendationView/ShadeRecommendationViewmodel.swift
@@ -1,2 +1,28 @@
 
 
+import SwiftUI
+import SwiftData
+
+final class ShadeRecommendationViewModel: ObservableObject {
+    @Published var showUnfavoriteAlert: Bool = false
+    @Published var isFavorite: Bool = false
+    @Published var showUnfavoriteDialog : Bool = false
+    let recommendation: ShadeRecommendation
+    
+    init(recommendation: ShadeRecommendation) {
+        self.recommendation = recommendation
+    }
+    
+    func toggleFavorite() {
+        if isFavorite {
+            showUnfavoriteAlert = true
+        } else {
+            isFavorite.toggle()
+        }
+    }
+
+    func confirmRemoveFavorite() {
+        isFavorite = false
+        showUnfavoriteAlert = false
+    }
+}
